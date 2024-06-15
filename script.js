6d493dfc8a9bbd727016e01f7dda47b3b9822ec8
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-06-16 00:09:20 +0300

    Display Movements Update

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 const displayMovements = function(movements)
 {
+    containerMovements.innerHTML='';
     movements.forEach(function(mov, i)
     {
         const type = mov > 0 ? 'deposit':'withdrawal';
