0e33243b8e2f62f024cb85adb7c8d610e5c255fe
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-06-16 16:25:17 +0300

    DisplayBalance

--- script.js
+++ script.js
@@ -79,6 +79,14 @@ const displayMovements = function(movements)
 
 displayMovements(account1.movements);
 
+const CalcDisplayBalance = function(movements)
+{
+    const balance = movements.reduce((acc, mov) => acc + mov , 0);
+    labelBalance.textContent= `${balance} $`;
+}
+
+CalcDisplayBalance(account1.movements)
+
 const user ='Luss Huguette Chanelle';
 const username = function(user)
 {
