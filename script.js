cc4bf678c18275d88427abce25707b7215e7504c
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-06-17 19:53:00 +0300

    Update Date logic

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ const account1 = {
       '2024-04-01T10:17:24.185Z',
       '2024-05-08T14:11:59.604Z',
       '2024-05-27T17:01:17.194Z',
-      '2024-07-11T23:36:17.929Z',
-      '2024-07-12T10:51:36.790Z',
+      '2024-06-10T23:36:17.929Z',
+      '2024-06-16T10:51:36.790Z',
     ],
     currency: 'TRY',
     locale: 'tr-TR',
@@ -35,8 +35,8 @@ const account2 = {
       '2023-01-25T14:18:46.235Z',
       '2024-02-05T16:33:06.386Z',
       '2024-04-10T14:43:26.374Z',
-      '2024-06-25T18:49:59.371Z',
-      '2024-07-26T12:01:20.894Z',
+      '2024-06-03T18:49:59.371Z',
+      '2024-07-16T12:01:20.894Z',
     ],
     currency: 'USD',
     locale: 'en-US',
@@ -71,6 +71,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+const formatMovementDate = function(date)
+{
+    const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+    const daysPassed = calcDaysPassed(new Date(), date);
+    console.log(daysPassed);
+
+    if (daysPassed === 0) return "Today";
+    if (daysPassed === 1) return "Yesterday";
+    if (daysPassed <= 7) return `${daysPassed} days ago`;
+    else
+    {
+        const day = `${date.getDate()}`.padStart(2, 0);
+        const month = `${date.getMonth() + 1}`.padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    }
+};
+
 const displayMovements = function(account, sort = false)
 {
     containerMovements.innerHTML='';
@@ -80,12 +99,10 @@ const displayMovements = function(account, sort = false)
     movs.forEach(function(mov, i)
     {
         const type = mov > 0 ? 'deposit':'withdrawal';
-        
+
         const date = new Date(account.movementsDates[i]);
-        const day = `${date.getDate()}`.padStart(2, 0);
-        const month = `${date.getMonth() + 1}`.padStart(2, '0');
-        const year = date.getFullYear();
-        const displayDate = `${day}/${month}/${year}`;
+        const displayDate = formatMovementDate(date);
+        
 
         const html = `
         <div class="movements__row">
