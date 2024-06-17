03dafe39e4c9463e6e2b7c828ad0f432f643a8d7
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-06-17 10:55:02 +0300

    Request Loan

--- script.js
+++ script.js
@@ -189,7 +189,22 @@ btnClose.addEventListener('click', function(e)
         // Hide UI
         containerApp.style.opacity = 0; 
         labelWelcome.textContent = 'Log in to get started';
-        console.log('Account Deleted');
     }
 
-})
+});
+
+btnLoan.addEventListener('click', function(e)
+{
+    e.preventDefault();
+
+    const amount = Number(inputLoanAmount.value);
+
+    if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1))
+    {
+        currentAccount.movements.push(amount);
+
+        updateUI(currentAccount);
+    };
+
+    inputLoanAmount.value='';
+});
